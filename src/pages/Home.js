889fff1a715d311bad3680d49104a00995edddcc
Barbaros Yıldırım <barbarosayildirim@gmail.com>
2021-09-15 15:47:53 +0300

    Fix HomePage
    Fixed text on homepage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,16 @@ import styled from "styled-components";
 
 const GreetingContainer = styled.div`
   padding: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Welcome = styled.h1`
   font-size: 4rem;
   text-decoration: none;
   font-family: "Poppins", sans-serif;
+  text-align: center;
 `;
 
 const WelcomeText =
